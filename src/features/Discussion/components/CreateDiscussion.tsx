f67ac3539fd5b7bbb0768ec938e7d2fcba8c28d4
Trương Quốc Trường <tqtolympia@gmail.com>
2025-06-10 16:45:01 +0700

    chore: delete redundant file at project

--- src/features/Discussion/components/CreateDiscussion.tsx
+++ src/features/Discussion/components/CreateDiscussion.tsx
@@ -13,7 +13,7 @@ import {
 } from "@mui/material";
 import { ArrowBack, Send } from "@mui/icons-material";
 import { useAuth } from "../../../context/AuthContext";
-import { discussionService } from "../../../services/discussionService";
+import { discussionService } from "../services/discussionService";
 
 const CreateDiscussion = () => {
   const navigate = useNavigate();
